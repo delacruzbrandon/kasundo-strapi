b9c39af02b9de651c1b3a8399669a0e737a6e7b6
delacruzbrandon <dcbrh.ph@gmail.com>
2026-01-12 17:32:58 +0800

    GraphQL Config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default ({ env }) => ({
+    graphql: {
+        config: {
+            endpoint: '/graphql',
+            shadowCRUD: true,
+            playgroundAlways: true, // Optional: keeps the web UI active
+            apolloServer: {
+                introspection: true, // <--- THIS IS THE FIX
+            },
+        },
+    },
+});
